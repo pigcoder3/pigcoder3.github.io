62a4f3d203d0b011e12c0bbd885d2e67833fbd07
pigcoder3 <pigcoder3@gmail.com>
2020-08-02 17:03:45 -0400

    Added the textfighter documentation and a link to it

---
+++ projects/textfighter/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.seanjohnson.textfighter"},{"l":"com.seanjohnson.textfighter.display"},{"l":"com.seanjohnson.textfighter.enemy"},{"l":"com.seanjohnson.textfighter.item"},{"l":"com.seanjohnson.textfighter.location"},{"l":"com.seanjohnson.textfighter.method"}]
