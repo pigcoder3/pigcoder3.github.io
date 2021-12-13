996327a6e43d6b1a6d643cfd3d69d1a0a7847a5b
Sean Johnson <seanmjohns1@gmail.com>
2021-12-12 20:01:48 -0500

    Added all experience to resume

--- projects/textfighter/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.seanjohnson.textfighter"},{"l":"com.seanjohnson.textfighter.display"},{"l":"com.seanjohnson.textfighter.enemy"},{"l":"com.seanjohnson.textfighter.item"},{"l":"com.seanjohnson.textfighter.location"},{"l":"com.seanjohnson.textfighter.method"}]
--- stickycontact.js
+++ stickycontact.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() { console.log("loading"); navbar = document.getElementById("navbar"); spacer = document.getElementById("spacer"); sticky = navbar.offsetTop;})
+document.addEventListener('DOMContentLoaded', function() { console.log("loading"); contactbar = document.getElementById("contactbar"); spacer = document.getElementById("spacer"); sticky = contactbar.offsetTop;})
 
 // Get the navbar
-var navbar;
+var contactbar;
 var spacer;
 var loaded;
 
@@ -11,12 +11,12 @@ var sticky;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 window.onscroll = function check() {
     if(document.documentElement.clientWidth <= 500) { return; }
-	if(!navbar) { return; }
+	if(!contactbar) { return; }
   	if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky");
+		contactbar.classList.add("sticky");
 		spacer.classList.add("stickyspacer");
   	} else {
-    	navbar.classList.remove("sticky");
+    	contactbar.classList.remove("sticky");
 		spacer.classList.remove("stickyspacer");
   	}
 } 
